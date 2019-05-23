87c5efe2f02a96b2f3a190f5b052e50bbf8adc0f
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-23 05:43:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,6 +7,10 @@ def get_first_name_of_season_winner(data, season)
       break
     end
   end
+
+  first_name = name.split(" ").first.join
+  first_name
+  
 end
 
 def get_contestant_name(data, occupation)
